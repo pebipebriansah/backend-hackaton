cc4ad30e21ed981185f2dec4b4284b105c64f4d4
Pebi Pebriansah <pebipebriansah160200@gmail.com>
2025-06-02 14:54:43 +0700

    Update harga.py

--- app/crud/harga.py
+++ app/crud/harga.py
@@ -1,14 +1,13 @@
 from sqlalchemy.orm import Session
 from sqlalchemy import extract
 from app.models.harga import Harga
-from datetime import datetime, timedelta
+from datetime import datetime
 
 def get_harga_bulan_ini_dan_lalu(db: Session):
     today = datetime.today()
     bulan_ini = today.month
     tahun_ini = today.year
 
-    # Hitung bulan lalu
     if bulan_ini == 1:
         bulan_lalu = 12
         tahun_lalu = tahun_ini - 1
@@ -16,8 +15,12 @@ def get_harga_bulan_ini_dan_lalu(db: Session):
         bulan_lalu = bulan_ini - 1
         tahun_lalu = tahun_ini
 
-    # Ambil harga berdasarkan bulan sekarang dan bulan kemarin
     return db.query(Harga).filter(
-        (extract('month', Harga.tanggal) == bulan_ini) & (extract('year', Harga.tanggal) == tahun_ini) |
-        (extract('month', Harga.tanggal) == bulan_lalu) & (extract('year', Harga.tanggal) == tahun_lalu)
+        (
+            (extract('month', Harga.created_at) == bulan_ini) &
+            (extract('year', Harga.created_at) == tahun_ini)
+        ) | (
+            (extract('month', Harga.created_at) == bulan_lalu) &
+            (extract('year', Harga.created_at) == tahun_lalu)
+        )
     ).all()
