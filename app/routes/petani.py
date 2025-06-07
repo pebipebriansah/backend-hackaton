646264eb3df1a17c5532fab58c0647212f55b118
Pebi Pebriansah <pebipebriansah160200@gmail.com>
2025-06-08 00:06:05 +0700

    Update petani.py

--- app/routes/petani.py
+++ app/routes/petani.py
@@ -7,7 +7,7 @@ from passlib.context import CryptContext
 from app.database import get_db, SessionLocal
 from app.schemas.petani import PetaniRegister, PetaniLogin, PetaniResponse, TokenWithInfo, PetaniUpdate
 from app.models.petani import Petani
-from app.crud.petani import create_petani, get_petani_by_email
+from app.crud.petani import create_petani, get_petani_by_email, update_petani
 from app.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
 router = APIRouter()
@@ -62,5 +62,5 @@ def update_petani(id_petani: int, petani_data: PetaniUpdate, db: Session = Depen
     if not db_petani:
         raise HTTPException(status_code=404, detail="Petani not found")
 
-    updated_petani = create_petani(db, petani_data)
+    updated_petani = update_petani(db, petani_data)
     return updated_petani
