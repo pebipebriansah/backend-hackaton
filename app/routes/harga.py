76c8c7a3b01a141de27964ed131e099f7491c84b
Pebi Pebriansah <pebipebriansah160200@gmail.com>
2025-06-18 10:11:02 +0700

    Update harga.py

--- app/routes/harga.py
+++ app/routes/harga.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from app.schemas.harga import HargaCabaiResponse, HargaCabai
 from app.scrapper.scrapper import ambil_harga_cabai
+from pydantic import ValidationError
 
 router = APIRouter()
 
@@ -10,5 +11,11 @@ def get_harga_cabai():
     if not data_scraped:
         raise HTTPException(status_code=500, detail="Gagal mengambil data harga cabai.")
     
-    harga_cabai_list = [HargaCabai(**item) for item in data_scraped]
+    try:
+        # Validasi dan konversi ke dict sesuai skema Pydantic
+        harga_cabai_list = [HargaCabai(**item).dict() for item in data_scraped]
+    except ValidationError as e:
+        # Tambahkan log di sini jika perlu
+        raise HTTPException(status_code=500, detail="Validasi data harga cabai gagal.")
+
     return {"data": harga_cabai_list}
